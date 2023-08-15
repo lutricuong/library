0f8d64ffb592c5134122614f55a6ce5bc286a336
lutricuong <tcuong.lu@gmail.com>
2023-08-15 22:25:06 +0700

    done basic addbook to library js

--- script.js
+++ script.js
@@ -2,27 +2,37 @@ const getTitle = document.querySelector('#title');
 const getAuthor = document.querySelector('#author');
 const getPages = document.querySelector('#pages');
 const getRead = document.getElementsByName('read');
-console.log(getReadStatus());
+const showDialog = document.querySelector('.showDialog');
+const closeDialog = document.querySelector('.closeDialog');
+console.log(closeDialog);
 
 let myLibrary = [
     {
         title: 'text',
         author: 'cuong lu',
         pages: '55',
-        read: 'read'
+        read: 'yes'
     }
 ];
-console.log(myLibrary);
 
-function book(title, author, pages) {
+closeDialog.addEventListener('click', addBook)
 
+function book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
-function addBook() {
+function addBook(e) {
+    e.preventDefault();
     title = getTitle.value;
     author = getAuthor.value;
     pages = getPages.value;
     read = getReadStatus();
+    const newBook = new book(title, author, pages, read);
+    myLibrary.push(newBook);
+    console.log(myLibrary);
 }
 
 function getReadStatus() {
